4b6acda7b885b4c538fe2c5c5045f82ecd34e43f
Ardjan Doci <ardjan.doci@student.supsi.ch>
2019-09-30 09:27:33 +0200

    Visual Studio test

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "test.cpp"
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+	printf("Hello, World!\n");
+	printf("%d\n", sum(3, 4));
+	int a;
+	scanf(&a);
+	return 0;
 }
+ 
